f5bad92e9dd4af287c8baea5e9b1ae1d61a12477
lanourain <lanourain@gmail.com>
2025-08-04 07:49:27 +0800

    add

--- csv_demo/csv_read.py
+++ csv_demo/csv_read.py
@@ -1,4 +1,16 @@
 import csv
+
+# 推荐写法
+# 以dict方式获取csv结果，自动跳过title行
+with open('for_csv_demo_test.csv', 'r') as c:
+    reader = csv.DictReader(c)
+    # reader = csv.DictReader(c, ['A', 'B', 'C'])  没有标题也可以预设标题
+    for row in reader:
+        for column in row:
+            print(row[column])
+
+print('------------')
+# 以list方式获取csv结果，适合没有title的csv，有title的需要跳过第一行
 with open('for_csv_demo_test.csv', 'r') as c:
     reader = csv.reader(c)
     csv_data = list(reader)
--- csv_demo/csv_write.py
+++ csv_demo/csv_write.py
@@ -1,4 +1,14 @@
 import csv
+# 推荐写法
+with open('for_csv_demo_test.csv', 'w', newline='') as csvfile:
+    # 指定title，写入title
+    outputDictWriter = csv.DictWriter(csvfile, ['Na', 'Pe', 'Ph'])
+    outputDictWriter.writeheader()
+    outputDictWriter.writerow({'Na':'N1', 'Pe': 'P1', 'Ph': 'Ph1'})
+
+
+print('------------')
+
 # 追加模式打开文件
 with open('for_csv_demo_test.csv', 'a', newline='') as csvfile:
     # 创建 csv 写对象
